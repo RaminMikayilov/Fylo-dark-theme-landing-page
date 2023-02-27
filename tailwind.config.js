a1b1b47341f5f6495d4a174d0b17ca87fd0aa6a4
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-02-28 00:23:26 +0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "375px",
+      xl: "1440px",
+    },
+    colors: {
+      darkBlue: {
+        100: "hsl(217, 28%, 15%)",
+        200: "hsl(218, 28%, 13%)",
+        300: "hsl(216, 53%, 9%)",
+        400: "hsl(219, 30%, 18%)",
+      },
+      cyan: "hsl(176, 68%, 64%)",
+      blue: "hsl(198, 60%, 50%)",
+      red: "hsl(0, 100%, 63%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontSize: {
+      sm: "14px",
+      md: "20px",
+      xl: "24px"
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+      openSans: ["Open Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
